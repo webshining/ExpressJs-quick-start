1dd63f83530e65f4e1f57cde50d2d0758858fc56
WebShining <79418317+webshining@users.noreply.github.com>
2022-07-09 19:23:46 +0300

    Update logging setting

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 import {Sequelize} from 'sequelize'
-import {DB_URI} from './config/config.js'
+import {DB_URI, LOGGING} from './config/config.js'
 
-const sequelize = new Sequelize(DB_URI)
+const sequelize = new Sequelize(DB_URI, {logging: LOGGING})
 
 export default sequelize
