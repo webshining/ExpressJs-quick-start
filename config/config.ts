294bd41de4d604a84b347c1f66c373f63ca78943
WebShining <79418317+webshining@users.noreply.github.com>
2022-07-09 19:53:57 +0300

    Update config

--- config/config.ts
+++ config/config.ts
@@ -8,7 +8,9 @@ const DB_NAME = process.env.DB_NAME || 'database'
 const DB_USER = process.env.DB_USER 
 const DB_PASS = process.env.DB_PASS 
 const DB_PORT = process.env.DB_PORT 
-const DB_HOST = process.env.DB_HOST 
+const DB_HOST = process.env.DB_HOST
+
+const LOGGING = process.env.LOGGING || false
 
 
 // Create database uri with existing data
@@ -18,4 +20,4 @@ if (DB_USER && DB_PASS && DB_HOST && DB_PORT && DB_NAME) {
     DB_URI = `postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
 }
 
-export {DB_URI, PORT}
+export {DB_URI, PORT, LOGGING}
