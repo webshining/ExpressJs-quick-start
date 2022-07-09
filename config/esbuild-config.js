73279ee6210dbb70ac8d49a9877683d3650ddf19
WebShining <79418317+webshining@users.noreply.github.com>
2022-07-09 19:24:27 +0300

    Update build config

--- config/esbuild-config.js
+++ config/esbuild-config.js
@@ -1,12 +1,13 @@
 import * as ESBuild from 'esbuild'
 import * as path from 'path'
+import {BUILD_FORMAT} from './config.js'
 
 ESBuild.build({
+    platform: 'node',
     outdir: path.resolve('./dist'),
     entryPoints: [path.resolve('./index.js')],
-    entryNames: 'bundle.js',
+    entryNames: 'bundle',
     bundle: true,
-    minify: true,
-    sourcemap: true,
-    external: ['./node_modules/*']
+    external: ['./node_modules/*'],
+    format: BUILD_FORMAT
 })
