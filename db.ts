f7ecaf143f6e04218a3f43ebb3febfeabce45d98
WebShining <79418317+webshining@users.noreply.github.com>
2022-07-09 19:54:08 +0300

    Update db config

--- db.ts
+++ db.ts
@@ -1,6 +1,8 @@
 import {Sequelize} from 'sequelize'
-import {DB_URI} from './data/config'
+import {DB_URI, LOGGING} from './config/config'
 
-const sequelize = new Sequelize(DB_URI)
+const sequelize = new Sequelize(DB_URI, {
+    logging: LOGGING as boolean
+})
 
 export default sequelize
