fe98a2bc9d831b7280eef9a78ebb649ea5e55bba
webshining <webshinings@gmail.com>
2022-11-02 15:41:17 +0200

    update variables type

--- config/config.ts
+++ config/config.ts
@@ -3,18 +3,18 @@ dotenv.config()
 
 const PORT = process.env.PORT || 7000
 
-const DB_NAME = process.env.DB_NAME || 'database'
+const DB_NAME: string = process.env.DB_NAME || 'database'
 
-const DB_USER = process.env.DB_USER 
-const DB_PASS = process.env.DB_PASS 
-const DB_PORT = process.env.DB_PORT 
-const DB_HOST = process.env.DB_HOST
+const DB_USER: string = process.env.DB_USER
+const DB_PASS: string = process.env.DB_PASS
+const DB_PORT: string = process.env.DB_PORT
+const DB_HOST: string = process.env.DB_HOST
 
-const LOGGING = process.env.LOGGING || false
+const LOGGING: boolean = process.env.LOGGING === 'true'
 
 
 // Create database uri with existing data
-let DB_URI = `sqlite://data/${DB_NAME}.sqlite`
+let DB_URI: string = `sqlite://data/${DB_NAME}.sqlite`
 
 if (DB_USER && DB_PASS && DB_HOST && DB_PORT && DB_NAME) {
     DB_URI = `postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}`
